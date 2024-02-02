41315ecfe3df70877ccbf6e6fc2bfbb535121c63
taihsin <taihsinlee@kkbox.com>
2024-02-02 16:20:24 +0800

    [chore]: Update Package.swift setting

--- Package.swift
+++ Package.swift
@@ -1,7 +1,31 @@
+// swift-tools-version:5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
 	name: "CommonCrypto",
-	targets: [],
-	dependencies: []
+	products: [
+		.library(
+			name: "CommonCrypto",
+			targets: ["CommonCrypto"]
+		),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+	],
+	targets: [
+		.systemLibrary(
+			name: "CLibrary",
+			pkgConfig: "clibrary",
+			providers: [
+				.brew(["clibrary"]),
+				.apt(["clibrary-dev"])
+			]
+		),
+		.target(
+			name: "CommonCrypto",
+			dependencies: [.target(name: "CLibrary") ]
+		),
+	]
 )
